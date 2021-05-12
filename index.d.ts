e9aa8b9ccff49cebeac509fe3d526abdba6f0d2c
Steve Thompson <writetome51@gmail.com>
2021-05-12 11:40:17 -0600

    Added typescript declarations

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+export declare function getRandomInteger(
+	[min, max]: [number, number]
+): number;
+
+
+export declare function getRandomIntegers(
+	howMany: number, 
+	[min, max]: [number, number]
+): number[];
